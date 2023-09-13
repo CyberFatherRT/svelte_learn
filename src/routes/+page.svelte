d9f62410d976f940a91556f8c496fa0af82dd693
cyberfather <maks12ate@gmail.com>
2023-09-13 19:54:16 +0000

    da

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+    import { clipboard } from '@skeletonlabs/skeleton';
+
+    const string: string = "never gonna give you up";
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button use:clipboard={string}>Copy</button>
